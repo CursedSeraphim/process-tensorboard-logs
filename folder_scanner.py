cb8c8560d36192c4d838b44ff0cefe3b46374bcf
cursedseraphim <cat-noir@gmx.at>
2021-06-05 10:12:40 +0200

    colour, titles, smoothing, logs, plots

--- folder_scanner.py
+++ folder_scanner.py
@@ -11,8 +11,23 @@ parser.add_argument('--logs_dir', default='logs',
                     help='each first level subdirectory within this logs_dir is used to create 1 plot consisting of the aggregated logs of all second level subdirectories within')
 parser.add_argument('--save_dir', default='plots',
                     help='directory to store matplotlib .png files')
+parser.add_argument('--c', default=None, nargs='+', action='append',
+                    help='like --c blue red --c blue')
+parser.add_argument('--xmax', default=int(2e6), type=int,
+                    help='extent of x axis')
+parser.add_argument('--n_samples', default=1000, type=int,
+                    help='amount of samples. for each sample in a plot it will move xmax/n_samples along the x-axis')
+parser.add_argument('--figsize', default=(6,4), nargs='+',
+                    help='maptlotlib figsize as in (18, 12), use --figsize 18 12')
+parser.add_argument('--smoothing', default=0.6, type=float,
+                    help='tb like smoothing weight')
+parser.add_argument('--titles', default=None, type=str, nargs='+',
+                    help='plot titles like --titles plot1 plot2')
 
 args = parser.parse_args()
+
+args.figsize = tuple([int(i) for i in args.figsize])
+print('figsize',args.figsize)
 
 # 'plots/' and 'plots' should both be accepted
 logs_dir = args.logs_dir+'/'
@@ -41,4 +56,4 @@ base_paths = [logs_dir for b in base]
 # create plots #
 ################
 
-create_plots(paths=paths, legend_labels=legend_labels, tags=tags, save_dirs=save_dirs, base_paths=base_paths)
+create_plots(paths=paths, legend_labels=legend_labels, tags=tags, save_dirs=save_dirs, base_paths=base_paths, colours=args.c, xmax=args.xmax, figsize=args.figsize, n_samples=args.n_samples, smoothing=args.smoothing, titles=args.titles)
